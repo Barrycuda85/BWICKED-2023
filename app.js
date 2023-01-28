e1c88f6ff5faf95f536eb2027bb7de506aeebe45
Barrycuda85 <bigbarry04@gmail.com>
2023-01-27 20:16:34 -0600

    Update app.js for ASSIGNMENT 2.6

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log ("Hey Dev Ed!")
+var life=100; //  this is NUMBER
+var name='Barry Wickham Jr'; // this is STRING
+
+let x = 88; // this is GLOBAL SCOPE
+
+var y = 6; // this is GLOBAL SCOPE 
+
+let b = "Very Nice!" // this is a STRING
+
+
+let z = x + y // this is an EQUATION
+{let x=678;} // this is BLOCK SCOPE
+
+const bassGuitar = { type: "Ibanez", model:"Sound Gear", color: "Mahogany" } // this is OBJECT VARIABLE
+
+console.log (b, x, y, z, life, name, bassGuitar,);
