a132140b0a9997a89e93c2d03585b39cf78abf8f
Barrycuda85 <bigbarry04@gmail.com>
2023-02-03 19:14:47 -0600

    ASSIGNMENT 3.4
    Added 2 functions with parameters that are triggered by onclick events in HTML. Added a method to existing object and called in through an onclick event in HTML

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+
+function mathFun(t1, t2) { 
+    return t1 * t2;
+}   // FUNCTION 1 TRIGGERED BY EVENT
+
+function myTime () {  
+    return Date();
+} // FUNCTION 2 TRIGGERED BY EVENT
+
+
 var life=100; //  this is NUMBER
 var name='Barry Wickham Jr'; // this is STRING
 
@@ -11,6 +21,13 @@ let b = "Very Nice!" // this is a STRING
 let z = x + y // this is an EQUATION
 {let x=678;} // this is BLOCK SCOPE
 
-const bassGuitar = { type: "Ibanez", model:"Sound Gear", color: "Mahogany" } // this is OBJECT VARIABLE
+const bassGuitar = { 
+    type: "Ibanez", 
+    model:"Sound Gear", 
+    color: "Mahogany",
+    style: "5-string",
+    fullDesc: function () {
+      return  this.type + "  " + this.model + " a " + this.style + " " + this.color + " color" + " bass guitar";}
+};// OBJECT with METHOD TRIGGERED BY EVENT
 
-console.log (b, x, y, z, life, name, bassGuitar,);
+console.log (b, x, y, z, life, name, bassGuitar, myTime, mathFun,bassGuitar);
